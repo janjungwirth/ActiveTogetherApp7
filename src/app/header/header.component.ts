0e4539a7ddb850ad5678290e47fd55338c29ee80
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2025-01-22 22:33:59 +0100

    V3.2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
     selector: 'app-header',
-  imports: [RouterModule, MatButton],
+  imports: [RouterModule, MatButton, MatTooltip],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
